075a5db2ceb8fe5b5f3844c4d02c4921adc3b9cc
Jangmin Lee <jang.xmin@gmail.com>
2022-01-12 23:08:25 +0900

    chore: eslint, prettier 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const App = () => {
-  return <div>Hello, React-App</div>;
-};
+function App() {
+	return <div>Hello, React-App</div>;
+}
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,51 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-  resolve: {
-    modules: [__dirname, 'src', 'node_modules'],
-    extensions: ['*', '.js', '.jsx'],
-  },
-  devServer: {
-    client: {
-      overlay: true,
-      progress: true,
-    },
-    hot: true,
-    open: true,
-    port: 3000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: require.resolve('babel-loader'),
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.png|svg|jpg|gif$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-          },
-        },
-      },
-    ],
-  },
+	mode: "development",
+	entry: "./src/index.jsx",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./public/index.html",
+		}),
+	],
+	resolve: {
+		modules: [__dirname, "src", "node_modules"],
+		extensions: ["*", ".js", ".jsx"],
+	},
+	devServer: {
+		client: {
+			overlay: true,
+			progress: true,
+		},
+		hot: true,
+		open: true,
+		port: 3000,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: require.resolve("babel-loader"),
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.png|svg|jpg|gif$/,
+				use: {
+					loader: "file-loader",
+					options: {
+						name: "[name].[ext]",
+					},
+				},
+			},
+		],
+	},
 };
