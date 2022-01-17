f621dbb440537c18fa21b7459cac71c3cad12768
Jangmin Lee <jang.xmin@gmail.com>
2022-01-18 02:19:28 +0900

    feat: CardButton 스타일 적용

--- src/components/CardButton/index.jsx
+++ src/components/CardButton/index.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 const CardButton = ({ onClick }) => {
 	return (
-		<button className="card-box" onClick={onClick}>
-			<div className="empty-card">+</div>
-		</button>
+		<StyledCardButton className="empty-card" onClick={onClick}>
+			+
+		</StyledCardButton>
 	);
 };
+
+const StyledCardButton = styled.button`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	width: 208px;
+	height: 130px;
+
+	font-size: 30px;
+	color: #575757;
+
+	background: #e5e5e5;
+	box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.25);
+	border-radius: 5px;
+
+	user-select: none;
+`;
 
 CardButton.propTypes = {
 	onClick: PropTypes.func.isRequired,
