90db975cdd38b27239efe378f8a23448d236ac62
PAPICO <ocipap0531@gmail.com>
2022-01-17 23:35:35 +0900

    카드번호 입력 받는 로직 구현

--- src/pages/AddCard/index.tsx
+++ src/pages/AddCard/index.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { EmptyCardItem } from "../../components/EmptyCardItem";
 import { Link } from "react-router-dom";
 
 export default function AddCardPage() {
-  return <div className="root">
-    <div className="app">
 
+  const [cardNumber, setCardNumber] = useState({
+    number1: '',
+    number2: '',
+    number3: '',
+    number4: '',
+  });
+
+  const handleChangeCardNumber: React.ChangeEventHandler<HTMLInputElement> = ({ target }) => {
+    const { id, name, value, maxLength } = target;
+
+    if (value.length >= maxLength) {
+      const arr = id.split('-'), nextId = parseInt(arr[arr.length - 1]);
+
+      const nextCardNumberInput = document.querySelector(`input#input-card-number-${nextId + 1}`);
+
+      if (nextCardNumberInput) {
+        (nextCardNumberInput as HTMLInputElement).select();
+      }
+    }
+
+    setCardNumber(prevState => ({ ...prevState, [name]: value }))
+  };
+
+
+  return <div className="root">
+    <form className="app">
       <Link to={'/'}>
         <h2 className="page-title">
           {'< 카드 추가'}
@@ -13,12 +37,32 @@ export default function AddCardPage() {
       </Link>
       <EmptyCardItem/>
       <div className="input-container">
-        <span className="input-title">카드 번호</span>
+        <label htmlFor={'input-card-number-1'} className="input-title">카드 번호</label>
         <div className="input-box">
-          <input className="input-basic" type="text"/>
-          <input className="input-basic" type="text"/>
-          <input className="input-basic" type="password"/>
-          <input className="input-basic" type="password"/>
+          <input
+            id={'input-card-number-1'}
+            name={'number1'}
+            className="input-basic"
+            type="text"
+            onChange={handleChangeCardNumber}
+            value={cardNumber.number1}
+            maxLength={4}
+          />
+          <input
+            id={'input-card-number-2'}
+            className="input-basic" name={'number2'} value={cardNumber.number2}
+            onChange={handleChangeCardNumber}
+            type="text" maxLength={4}/>
+          <input
+            id={'input-card-number-3'}
+            className="input-basic" name={'number3'} value={cardNumber.number3}
+            onChange={handleChangeCardNumber}
+            type="password" maxLength={4}/>
+          <input
+            id={'input-card-number-4'}
+            className="input-basic" name={'number4'} value={cardNumber.number4}
+            onChange={handleChangeCardNumber}
+            type="password" maxLength={4}/>
         </div>
       </div>
       <div className="input-container">
@@ -50,6 +94,6 @@ export default function AddCardPage() {
       <div className="button-box">
         <span className="button-text">다음</span>
       </div>
-    </div>
+    </form>
   </div>
 }
