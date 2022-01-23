8a2e3c02dd4820405a2824f6338caa8980cb7421
sukyeongh <hsk10271@gmail.com>
2022-01-23 16:24:46 +0900

    feat : 만료일 컴포넌트 구현

---
+++ payments/src/components/InputForm/InputContainer/ValidDayInput/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as Styled from './style.js';
+
+export const ValidDayInputContainer = ({ validDay, handleChange }) => {
+  const { month, year } = validDay;
+  return (
+    <Styled.Container>
+      <Styled.Input name={'month'} value={month} onChange={handleChange} placeholder={'MM'} />
+      <span>/</span>
+      <Styled.Input name={'year'} value={year} onChange={handleChange} placeholder={'YY'} />
+    </Styled.Container>
+  );
+};
+
+ValidDayInputContainer.propTypes = {
+  validDay: PropTypes.object,
+};
+
+ValidDayInputContainer.defaultProps = {
+  validDay: { month: '10', year: '27' },
+};
