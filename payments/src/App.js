f8db9a4385215cd8f215e282adcf46187df326b9
sukyeongh <hsk10271@gmail.com>
2022-01-23 17:19:09 +0900

    feat : 카드 추가 페이지 구현

--- payments/src/App.js
+++ payments/src/App.js
@@ -1,5 +1,11 @@
+import { CardRegister } from './container/CardRegister';
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div className='App'>
+      <CardRegister />
+    </div>
+  );
 }
 
 export default App;
