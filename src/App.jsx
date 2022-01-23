cbba1ccfd21b47a7ac16ba83210c2fe12859f5cc
lisoh <sohyunlee0902@gmail.com>
2022-01-23 18:06:14 +0900

    1. 카드 번호입력 폼에 라벨을 보여준다.

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,15 @@ import AddCard from './components/AddCard';
 import ExpDate from './components/ExpDate';
 import CardOwnerName from './components/CardOwnerName';
 import CardSecurityCode from './components/CardSecurityCode';
+import CardPassword from './components/CardPassword';
 
 function divInputContainer({ children }) {
   return <div className="input-container">{children}</div>;
 }
+
 const App = () => {
   {
     /* 밑에 있는 컴포넌트들에게 공통적으로 바뀌는 상태값을 prop으로 내려주는 함수 만들기 */
-  }
-  function State() {
-    return state;
   }
 
   return (
--- src/components/CardNumbers.jsx
+++ src/components/CardNumbers.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 
+function checkNumber(event) {
+  if (event.key >= 0 && event.key <= 9) {
+    return true;
+  }
+  return null;
+}
+
 const CardNumbers = () => {
   return (
     <>
-      <span className="input-title">카드 번호</span>
+      <label htmlFor="CardNumber1" className="input-title">
+        카드 번호
+      </label>
       <div className="input-box">
-        <input className="input-basic" type="text" />
-        <input className="input-basic" type="text" />
-        <input className="input-basic" type="password" />
-        <input className="input-basic" type="password" />
+        <input
+          id="CardNumber1"
+          className="input-basic"
+          type="text"
+          maxLength={4}
+        />
+        <input className="input-basic" type="text" maxLength={4} />
+        <input
+          className="input-basic"
+          type="password"
+          maxLength={4}
+          onkeypress="return checkNumber(event)"
+        />
+        <input
+          className="input-basic"
+          type="password"
+          maxLength={4}
+          onkeypress="return checkNumber(event)"
+        />
       </div>
     </>
   );
