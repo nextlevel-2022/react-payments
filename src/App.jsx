5682687ea1508de9d5e3ae9edb8aac7f329fc079
lisoh <sohyunlee0902@gmail.com>
2022-01-24 09:46:57 +0900

    CardNumbers.jsx에 input의 handleChange함수 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CardNumbers from './components/CardNumbers';
 import AddCard from './components/AddCard';
 import ExpDate from './components/ExpDate';
@@ -14,17 +14,20 @@ const App = () => {
   {
     /* 밑에 있는 컴포넌트들에게 공통적으로 바뀌는 상태값을 prop으로 내려주는 함수 만들기 */
   }
+  const [inputs, setInputs] = useState({
+    cardNumber1: null,
+    cardNumber2: null,
+    cardNumber3: null,
+    cardNumber4: null,
+  });
 
   return (
     <div className="app">
       <AddCard />
-      {[
-        CardNumbers,
-        ExpDate,
-        CardOwnerName,
-        CardSecurityCode,
-        CardPassword,
-      ].map(Input => (
+      <div className="input-container">
+        <CardNumbers />
+      </div>
+      {[ExpDate, CardOwnerName, CardSecurityCode, CardPassword].map(Input => (
         <div className="input-container">
           <Input />
         </div>
--- src/components/CardNumbers.jsx
+++ src/components/CardNumbers.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-
-function checkNumber(event) {
-  if (event.key >= 0 && event.key <= 9) {
-    return true;
-  }
-  return null;
-}
+import React, { useState } from 'react';
 
 const CardNumbers = () => {
+  const [inputs, setInputs] = useState({
+    cardNumber1: null,
+    cardNumber2: null,
+    cardNumber3: null,
+    cardNumber4: null,
+  });
+
+  function handleChange(event) {
+    setInputs(old => ({ ...old, [event.target.name]: event.target.value }));
+  }
+  console.log(inputs);
   return (
     <>
       <label htmlFor="CardNumber1" className="input-title">
@@ -16,22 +20,36 @@ const CardNumbers = () => {
       <div className="input-box">
         <input
           id="CardNumber1"
+          name="cardNumber1"
+          onChange={handleChange}
           className="input-basic"
           type="text"
           maxLength={4}
-        />
-        <input className="input-basic" type="text" maxLength={4} />
-        <input
-          className="input-basic"
-          type="password"
-          maxLength={4}
-          onkeypress="return checkNumber(event)"
+          required
         />
         <input
           className="input-basic"
+          name="cardNumber2"
+          onChange={handleChange}
+          type="text"
+          maxLength={4}
+          required
+        />
+        <input
+          className="input-basic"
+          name="cardNumber3"
+          onChange={handleChange}
           type="password"
           maxLength={4}
-          onkeypress="return checkNumber(event)"
+          required
+        />
+        <input
+          className="input-basic"
+          name="cardNumber4"
+          onChange={handleChange}
+          type="password"
+          maxLength={4}
+          required
         />
       </div>
     </>
