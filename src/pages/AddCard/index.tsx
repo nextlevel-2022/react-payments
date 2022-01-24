64f30c6f089bfbbe75dba02b16ae8c0a4123868d
PAPICO <ocipap0531@gmail.com>
2022-01-24 12:34:35 +0900

    new: maxlength 입력 시 다음 입력 창 focus

--- src/pages/AddCard/index.tsx
+++ src/pages/AddCard/index.tsx
@@ -2,18 +2,29 @@ import React, { createRef } from "react";
 import { EmptyCardItem } from "../../components/EmptyCardItem";
 import { Link } from "react-router-dom";
 
-const cardNumberNames = ['cardNumber0', 'cardNumber1', 'cardNumber2', 'cardNumber3']
+const formNames = [
+  'cardNumber0',
+  'cardNumber1',
+  'cardNumber2',
+  'cardNumber3',
+  'mm',
+  'yy',
+  'owner',
+  'cvc',
+  'password0',
+  'password1',
+]
 
 export default function AddCardPage() {
-  const cardNumberRefs = cardNumberNames.map(() => createRef<HTMLInputElement>());
+  const formRefs = formNames.map(() => createRef<HTMLInputElement>());
 
-  const handleChangeCardNumber: React.ChangeEventHandler<HTMLInputElement> = ({ target }) => {
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = ({ target }) => {
     const { name, value, maxLength } = target;
 
-    if (value.length >= maxLength) {
-      const id = cardNumberNames.findIndex((_name) => _name === name);
+    if (maxLength && value.length >= maxLength) {
+      const id = formNames.findIndex((_name) => _name === name);
 
-      const $nextInput = cardNumberRefs[id + 1].current;
+      const $nextInput = formRefs[id + 1].current;
 
       if ($nextInput) {
         ($nextInput as HTMLInputElement).select();
@@ -35,56 +46,93 @@ export default function AddCardPage() {
         <div className="input-box">
           <input
             className="input-basic"
-            name={cardNumberNames[0]}
-            ref={cardNumberRefs[0]}
-            onChange={handleChangeCardNumber}
+            name={formNames[0]}
+            ref={formRefs[0]}
+            onChange={handleChange}
             type="text" maxLength={4}
           />
           <input
             className="input-basic"
-            name={cardNumberNames[1]}
-            ref={cardNumberRefs[1]}
-            onChange={handleChangeCardNumber}
+            name={formNames[1]}
+            ref={formRefs[1]}
+            onChange={handleChange}
             type="text" maxLength={4}/>
           <input
             className="input-basic"
-            name={cardNumberNames[2]}
-            ref={cardNumberRefs[2]}
-            onChange={handleChangeCardNumber}
+            name={formNames[2]}
+            ref={formRefs[2]}
+            onChange={handleChange}
             type="password" maxLength={4}/>
           <input
             className="input-basic"
-            name={cardNumberNames[3]}
-            ref={cardNumberRefs[3]}
-            onChange={handleChangeCardNumber}
+            name={formNames[3]}
+            ref={formRefs[3]}
+            onChange={handleChange}
             type="password" maxLength={4}/>
         </div>
       </div>
       <div className="input-container">
         <span className="input-title">만료일</span>
         <div className="input-box w-50">
-          <input className="input-basic" type="text" placeholder="MM"/>
-          <input className="input-basic" type="text" placeholder="YY"/>
+          <input
+            className="input-basic"
+            name={formNames[4]}
+            ref={formRefs[4]}
+            onChange={handleChange}
+            placeholder="MM"
+            type="text" maxLength={2}/>
+          <input
+            className="input-basic"
+            name={formNames[5]}
+            ref={formRefs[5]}
+            onChange={handleChange}
+            placeholder="YY"
+            type="text" maxLength={2}/>
         </div>
       </div>
       <div className="input-container">
         <span className="input-title">카드 소유자 이름(선택)</span>
         <input
-          type="text"
           className="input-basic"
+          name={formNames[5]}
+          ref={formRefs[5]}
           placeholder="카드에 표시된 이름과 동일하게 입력하세요."
+          type="text"
         />
       </div>
       <div className="input-container">
         <span className="input-title">보안코드(CVC/CVV)</span>
-        <input className="input-basic w-25" type="password"/>
+        <input
+          className="input-basic w-25"
+          name={formNames[6]}
+          ref={formRefs[6]}
+          onChange={handleChange}
+          type="password" maxLength={3}/>
       </div>
       <div className="input-container">
         <span className="input-title">카드 비밀번호</span>
-        <input className="input-basic w-15" type="password"/>
-        <input className="input-basic w-15" type="password"/>
-        <input className="input-basic w-15" type="password"/>
-        <input className="input-basic w-15" type="password"/>
+        <input
+          className="input-basic w-15"
+          name={formNames[7]}
+          ref={formRefs[7]}
+          onChange={handleChange} type="password" maxLength={1}/>
+        <input
+          className="input-basic w-15"
+          name={formNames[8]}
+          ref={formRefs[8]}
+          onChange={handleChange} type="password" maxLength={1}/>
+        <input
+          className="input-basic w-15"
+          type="password"
+          disabled
+          value="*"
+        />
+        <input
+          className="input-basic w-15"
+          type="password"
+          disabled
+          value="*"
+        />
       </div>
       <div className="button-box">
         <span className="button-text">다음</span>
