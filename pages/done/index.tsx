f0ba8cf7131b27f665ded2484c474d6383d35c0e
g1moon <gmoon.cho@gmail.com>
2022-01-29 15:13:05 +0900

    * feat: 카드 추가 완료를 보여주는 페이지 추가('/done'

---
+++ pages/done/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+
+const Done: NextPage = () => {
+  return (
+    <>
+      <h2>4️⃣ 카드 추가 완료</h2>
+      <h2 className="page-title mb-10">카드등록이 완료되었습니다.</h2>
+    </>
+  );
+};
+
+export default Done;
