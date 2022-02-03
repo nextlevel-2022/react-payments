b0d31ece92fcd233b6250279be55170431d99c2b
Wie Youngmin <50790145+youngminss@users.noreply.github.com>
2022-02-04 01:10:10 +0900

    feat: 카드 목록 페이지

--- src/pages/CardList/index.tsx
+++ src/pages/CardList/index.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import { CardData } from '../../common/constants';
 import Card from '../../components/Card';
-import AddCard from '../../components/Card/addCard';
+import DeletableCard from '../../components/Card/deletable';
+import { useCardList } from '../../contexts/cardList';
+import { useRouter } from '../../contexts/route';
 
 const CardList = () => {
+	const { setRoute } = useRouter();
+	const { cards, setEditingCard } = useCardList();
+
+	const handleClickCard = (card: CardData) => () => {
+		setEditingCard(card);
+		setRoute('CONFIRM');
+	};
+
 	return (
-		<div className="flex-column-center">
+		<div className="page-container flex-column-center">
 			<div className="flex-center">
 				<h2 className="page-title mb-10">보유 카드</h2>
 			</div>
-			<div className="flex-column-center">
-				<Card />
-				<span className="card-nickname">법인카드</span>
-			</div>
-			<AddCard />
+			{cards.map((card, i) => (
+				<DeletableCard key={i} type="small" cardData={card} onClick={handleClickCard(card)} />
+			))}
+			<Card type="new" card={null} />
 		</div>
 	);
 };
