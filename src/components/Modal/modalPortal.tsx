13e43cd32e04e92e7d163415c7eeb5e77c3c5c39
Wie Youngmin <50790145+youngminss@users.noreply.github.com>
2022-02-04 01:10:49 +0900

    feat: 카드사, 키패드 모달 창

---
+++ src/components/Modal/modalPortal.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+
+const ModalPortal = ({ children }: any) => {
+	const [elem, setElem] = useState<HTMLElement | null>(null);
+
+	useEffect(() => {
+		const $root = document.getElementById('modalRoot');
+
+		if (!$root) return;
+		setElem($root);
+		$root.classList.add('show');
+
+		return () => {
+			$root.classList.remove('show');
+		};
+	}, []);
+
+	return elem ? createPortal(children, elem) : null;
+};
+
+export default ModalPortal;
